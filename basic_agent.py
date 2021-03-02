8c5d34519b3b0714f652d2f52624a267645762e1
Em <dcshi6@gmail.com>
2021-03-01 20:09:45 -0500

    Corrected documentation/comments in basic agent

--- basic_agent.py
+++ basic_agent.py
@@ -4,21 +4,7 @@ from manual_agent import *
 from knowledge_base import *
 from mine_utils import *
 
-"""
-1. check is a given cell is safe or mine or currently covered
-    --write a check_cell method
-2. if safe check the number of neighbors that are mine. 
-    -- function to check the num of mines
-    -- function to check the num of safe cells
-    -- function to check the cell with num
-    -- function to check the hidden cells
-3. randomly choose
 
-
-"""
-
-
-# TODO: Fix infinite loop where I keep selecting the same element over and over again
 def basic_agent(board, kb, dim, n, p=False):
     """
         Automatically solve Minesweeper using the most basic way described by Dr Cowan
@@ -28,7 +14,7 @@ def basic_agent(board, kb, dim, n, p=False):
         :param dim: The dimensions of the board/knowledge base
         :param n: Number of mines, only used to determine when the game ends. The agent themself doesn't take this into account.
         :param p: If I want to print the final knowledge base, defaults to False
-        :return: Void, when the only uncovered elements are mines or entire board is searched
+        :return: A tuple of the format (number unrevealed mines, number total mines)
         """
     check = {}  # these are the REVEALED cells I have to check neighbors of
     fringe = {}  # these are UNREVEALED neighbors of cells I revealed
@@ -51,23 +37,25 @@ def basic_agent(board, kb, dim, n, p=False):
 
             if neighborMineCount - numRevealedNeighbors == numUnrevealedNeighbors:
                 markAllNeighborsDangerous(kb, dim, row, col)
-            elif numNeighbors - neighborMineCount - count_safe_revealed_neighbors(kb, dim, row, col) == numUnrevealedNeighbors:
+            elif numNeighbors - neighborMineCount - count_safe_revealed_neighbors(kb, dim, row,
+                                                                                  col) == numUnrevealedNeighbors:
                 markAllNeighborsSafe(kb, dim, row, col)
 
         fringe = addToFringe(kb, dim, row, col, fringe)
         cleanFringe(check, fringe, kb, board, row, col, dim)
         updateAllSafe(kb, board, dim)
-        if check:
+        if check:  # there's still a neighbor who I can check
             current = check.popitem()
             row = current[0][0]
             col = current[0][1]
         else:  # Must now randomly choose something from fringe, as I've exhausted all the conclusive elements
+            # The trouble starts here
             keys = list(fringe.keys())
             if keys:
                 randomKey = random.choice(keys)
                 fringe.pop(randomKey)
             else:
-                unrevealed = getAllUnrevealedinKB(kb, dim)
+                unrevealed = getAllUnrevealedInKB(kb, dim)
                 keys = list(unrevealed.keys())
                 randomKey = random.choice(keys)
                 unrevealed.pop(randomKey)
@@ -97,7 +85,9 @@ def addToFringe(kb, dim, row, col, fringe: dict):
     """
     neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
     for i in range(len(neighbors)):
-        if isValid(kb, dim, row + neighbors[i][0], col + neighbors[i][1]) and (kb[row + neighbors[i][0]][col + neighbors[i][1]] == '?' or kb[row + neighbors[i][0]][col + neighbors[i][1]] == 'S'):
+        if isValid(kb, dim, row + neighbors[i][0], col + neighbors[i][1]) and (
+                kb[row + neighbors[i][0]][col + neighbors[i][1]] == '?' or kb[row + neighbors[i][0]][
+            col + neighbors[i][1]] == 'S'):
             fringe[(row + neighbors[i][0], col + neighbors[i][1])] = kb[row + neighbors[i][0]][col + neighbors[i][1]]
     return fringe
 
@@ -159,7 +149,7 @@ def markAllNeighborsDangerous(kb, dim, row, col):
     return kb
 
 
-def getAllUnrevealedinKB(kb, dim):
+def getAllUnrevealedInKB(kb, dim):
     """
     Gets all unrevealed and unmarked elements of the kb
 
@@ -178,7 +168,7 @@ def getAllUnrevealedinKB(kb, dim):
 
 def updateAllSafe(kb, board, dim):
     """
-    Updates all safe elements in kb
+    Reveals all marked safe elements in kb
 
     :param kb: The knowledge base
     :param board:
